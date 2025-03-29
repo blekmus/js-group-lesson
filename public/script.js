8e93dbcdd4117d7c086eec6593c53567a3aa6549
Jakejellypie <vikramaadityamathur@gmail.com>
2025-03-29 19:43:50 -0400

    added socket.io initial implementation

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+const socket = io("http://localhost:3000/");
 
 //adding the username h2 element as a var to be updated
 const toBeUpdatedUserName = document.getElementById("username");
@@ -8,9 +9,8 @@ let userInput = document.getElementById("input")
 //creating a var for the message display area
 const msgDisplay = document.querySelector(".msgDisplay ul");
 
-
 //creating a username, whatever you want it to be
-let username = "Default";
+let username = "jakejellypie";
 //create your own custom description
 let description = "Hi my name is Vik, I like pokemon quite a bit :)"
 
@@ -20,29 +20,39 @@ toBeUpdatedUserName.innerText = "@" + username;
 toBeUpdatedDesc.innerText = description;
 
 
-
-input.addEventListener("keydown", (e) => {
+userInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
-        if (input.value.trim() !== "") {
+        if (userInput.value.trim() !== "") {
             e.preventDefault();
 
-            let newMessage = document.createElement("li");
-            newMessage.innerText = input.value;
+            const userMessage = userInput.value;
 
-            msgDisplay.prepend(newMessage);
-            input.value = "";
-
-            setTimeout(() => {
-                newMessage.classList.add("fade-in");
-
-                const msgContainer = document.querySelector(".msgDisplay");
-                //telling the document (the container of ul which is msgDisplay) to scroll down
-                //by making its current scrollTop property it's new maxheight (post new item's addition)
-                //scrollTop = 0 (top of the element), scrollTop = 100 (scroll down 100 pixels).
-                msgContainer.scrollTop = msgContainer.scrollHeight;
-            }, 100);
-        } else {
+            socket.emit('frontEndMsgReceived', ({username, message: userMessage}));
+            userInput.value = "";
+        } 
+        else 
+        {
             alert("Write something");
         }
     }
+});
+socket.on('backEndMsgSent', ({username, message})=>{
+    let newMessage = document.createElement("li");
+    const finalMessage = `@${username}<br>${message}`;
+    console.log("finalMessage is:\n" + finalMessage);
+    newMessage.innerHTML = finalMessage;
+    msgDisplay.prepend(newMessage);
+
+    setTimeout(() => {
+        newMessage.classList.add("fade-in");
+        //changes the color and the style of the username.
+        newMessage.classList.add("yourUsernameClass");
+            const msgContainer = document.querySelector(".msgDisplay");
+        
+    //     //telling the document (the container of ul which is msgDisplay) to scroll down
+    //     //by making its current scrollTop property it's new maxheight (post new item's addition)
+    //     //scrollTop = 0 (top of the element), scrollTop = 100 (scroll down 100 pixels).
+            msgContainer.scrollTop = msgContainer.scrollHeight;
+    }, 100);
+
 });
---
+++ test-server.js
@@ -0,0 +1,42 @@
+//importing express to link the frontend with the clients that log in using http module
+const expressImport = require("express");
+//loading the http module to let people connect to this test server (well just me rn )
+const httpImport = require("http");
+
+/*loading in socket.io module to manage communication b/w websockets, 
+ we will add this on top of the server created with http and express*/
+ const socketIO = require("socket.io");
+
+//creating an instance of the expressImport
+const app = expressImport();
+
+//creating a server by linking the app with the http module
+const server = httpImport.createServer(app);
+
+//providing the correct static files from express to the server, appropriate ui for the users to see when they access it through http
+app.use(expressImport.static('public'));
+
+//putting socket.io ontop of the server to manage communication between the sockets (each private connection) to enable real time connections
+//by creating an instance
+const io = socketIO(server);
+
+//using the inbuilt socketIO methods to check if user(s) have successfully connected
+io.on('connection', (socket) => {
+    console.log('A user has connected');
+
+    //creating a listener for message by the front-end to the backend server (test-server.js)
+    socket.on('frontEndMsgReceived', ({username, message})=>{
+        console.log("Server received frontEndMsgReceived:", {username, message});
+        socket.broadcast.emit('backEndMsgSent', ({username, message}));
+    });
+    
+    //attaching the unique user (socket) to a disconnect check
+    socket.on('disconnect', () => {
+        console.log("A user has disconnected");
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
